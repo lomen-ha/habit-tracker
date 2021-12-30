2d86c64ee012a39c58d6bbf713c61871dcfb9136
lomen-front <help.service.lomen@gmail.com>
2021-12-30 15:37:46 +0900

    habit-tracker : handleIncrement function

--- habit-tracker/src/components/habit.jsx
+++ habit-tracker/src/components/habit.jsx
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
-  state = {
-    count: 0,
+  handleIncrement = () => {
+    this.props.onIncrement(this.props.habit);
   };
 
-  handleIncrement = () => {
-    // state 오브젝트 안에 있는 count를 증가 한 뒤 state를 업데이트 해야 함.
-    this.setState({ count: this.state.count + 1 });
-  };
   handleDecrement = () => {
-    const count = this.state.count - 1;
-    this.setState({ count: count < 0 ? 0 : count });
+    this.props.onDecrement(this.props.habit.name);
+  };
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit.name);
   };
 
   render() {
@@ -32,7 +31,10 @@ class Habit extends Component {
         >
           <i className="fas fa-minus-square"></i>
         </button>
-        <button className="habit-button habit-delete">
+        <button
+          className="habit-button habit-delete"
+          onClick={this.handleDelete}
+        >
           <i className="fas fa-trash"></i>
         </button>
       </li>
--- habit-tracker/src/components/habits.jsx
+++ habit-tracker/src/components/habits.jsx
@@ -9,11 +9,32 @@ class Habits extends Component {
       { id: 3, name: 'Coding', count: 0 },
     ],
   };
+  handleIncrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits });
+  };
+
+  handleDecrement = (habit) => {
+    console.log(`handleDecrement ${habit}`);
+  };
+
+  handleDelete = (habit) => {
+    console.log(`handleDelete ${habit}`);
+  };
+
   render() {
     return (
       <ul>
         {this.state.habits.map((habit) => (
-          <Habit key={habit.id} habit={habit} />
+          <Habit
+            key={habit.id}
+            habit={habit}
+            onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
+            onDelete={this.handleDelete}
+          />
         ))}
       </ul>
     );
